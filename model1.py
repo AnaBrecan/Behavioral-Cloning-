c054d83ab23a9b02a22f07ff7d23220f6d095ac0
Ana-Maria Franzen-Brecan <anabrecan@gmail.com>
2021-09-07 16:25:16 +0200

    modified Conv

--- model1.py
+++ model1.py
@@ -16,42 +16,55 @@ def generator(samples, batch_size=32):
     while 1: # Loop forever so the generator never terminates
         shuffle(samples)
         for offset in range(0, num_samples, batch_size):
-            batch_samples = samples.iloc[offset:offset+batch_size, :]
-            
-            image_names = []
+            batch_samples = samples[offset:offset+batch_size]
+            images = []
             measurements = []
             
-            for index in batch_samples.index:
-                image = batch_samples.get_value(index, 0)
-                measurement = batch_samples.get_value(index, 3)
-                image_names.append('data/'+image)
+            for batch_sample in batch_samples:
+                
+                image_center = batch_sample[0]
+                image_left = batch_sample[1]
+                image_right = batch_sample[2]
+                
+                measurement = float(batch_sample[3])
+                
+                image_c = mpimg.imread('data/'+image_center)
+                images.append(image_c)
                 measurements.append(measurement)
                 
-            # Load all images
-            images = []
-            for i in image_names:
-                image = mpimg.imread(i)
-                images.append(image)
+                image_l = mpimg.imread('./data/IMG/' + batch_sample[1].split('/')[-1])
+                images.append(image_l)
+                measurements.append(measurement + 0.35)
+                
+                image_r = mpimg.imread('./data/IMG/' + batch_sample[2].split('/')[-1])
+                images.append(image_r)
+                measurements.append(measurement - 0.35)
+                
+                images.append(cv2.flip(image_c, 1))
+                measurements.append(float(measurement) * (-1.0))
+
+                images.append(cv2.flip(image_l, 1))
+                measurements.append(float(measurement) * (-1.0) + 0.35)
+                
+                images.append(cv2.flip(image_r, 1))
+                measurements.append(float(measurement) * -1.0 - 0.35)  
+
+
             
-            augmented_images=[]
-            augmented_measurements=[]
-
-            for image, measurement in zip(images, measurements):
-                augmented_images.append(image)
-                augmented_measurements.append(measurement)
-                augmented_images.append(cv2.flip(image,1))
-                augmented_measurements.append(float(measurement)*(-1.0))
-
-            # trim image to only see section with road
-            X_train = np.array(augmented_images)
-            y_train = np.array(augmented_measurements)
+            X_train = np.array(images)
+            y_train = np.array(measurements)
             yield sklearn.utils.shuffle(X_train, y_train)
 
 
 
-driving_data = pd.read_csv('data/driving_log.csv', sep=',', header = None)
+driving_data = []
+with open('data/driving_log.csv') as file:
+    reader = csv.reader(file)
+    next(reader)
+    for row in reader:
+        driving_data.append(row)
 
-# Set our batch size
+# Set the batch size
 batch_size=32
 
 from sklearn.model_selection import train_test_split
@@ -71,51 +84,36 @@ from keras.layers import Cropping2D
 model = Sequential()
 
 model.add(Lambda(lambda x: (x/255.0)-0.5, input_shape=(160,320,3)))
+# trim image to only see section with road
 model.add(Cropping2D(cropping=((75,25),(0,0))))
 
-model.add(Conv2D(6,(5,5)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D((2,2)))
-model.add(Dropout(0.5))
-          
-          
-model.add(Conv2D(16,(5,5)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D((2,2)))
-model.add(Dropout(0.5))
-          
-model.add(Conv2D(8,(3,3)))
-model.add(Activation('relu'))         
-
-
+model.add(Conv2D(24, (5, 5), activation='relu', strides=(2, 2)))
+model.add(Conv2D(36, (5, 5), activation='relu', strides=(2, 2)))
+model.add(Conv2D(48, (3, 3), activation='relu', strides=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu', strides=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu', strides=(2, 2)))
+model.add(Dropout(0.3))
 model.add(Flatten())
-model.add(Dropout(0.5))
-model.add(Dense(120))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))          
-model.add(Dense(84))
-model.add(Activation('relu'))  
-model.add(Dropout(0.5))          
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(50, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(10, activation='relu'))
+model.add(Dropout(0.3))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-history_object = model.fit_generator(
+model.fit_generator(
     train_generator,
     steps_per_epoch=ceil(len(train_samples) / batch_size),
     validation_data=validation_generator,
     validation_steps=ceil(len(validation_samples) / batch_size),
-    epochs=10,
+    epochs=2,
     verbose=1,
     )
           
-#history_object = model.fit_generator(train_generator, samples_per_epoch =
- #   len(train_samples), validation_data = 
-  #  validation_generator,
-    #nb_val_samples = len(validation_samples), 
-   # nb_epoch=1, verbose=1)
 
-### print the keys contained in the history object
-print(history_object.history.keys())
-
-model.save('model1.h5')
+model.save('model6.h5')
+print("Model Saved")
+model.summary()
 
